e3e1e9de634d9e43abb5a9b7475ea801a3c12941
Junbo Wang <juniwang@microsoft.com>
2022-12-19 19:36:04 +0800

    support options

---
+++ src/ResourceCleaner/ResourceCleaner/AzureOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace ResourceCleaner
+{
+    internal class AzureOptions
+    {
+        public string TenantId { get; set; }
+        public string SubscriptionId { get; set; }
+        public string ClientId { get; set; }
+        public string ClientCertificate { get; set; }
+        public int TTLMinutes { get; set; }
+    }
+}
--- src/ResourceCleaner/ResourceCleaner/Program.cs
+++ src/ResourceCleaner/ResourceCleaner/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+
+namespace ResourceCleaner
+{
+    public class Program
+    {
+        const string Azure = "Azure";
+
+        public static void Main(string[] args)
+        {
+            var options = GetOptions(args);
+            Console.WriteLine($"TenantId: {options.TenantId}");
+            Console.WriteLine($"Cert: {options.ClientCertificate}");
+        }
+
+        private static AzureOptions GetOptions(string[] args)
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .AddCommandLine(args)
+                .AddUserSecrets(typeof(AzureOptions).Assembly)
+                .Build();
+
+            var azureOptions = new AzureOptions();
+            configuration.GetSection(Azure).Bind(azureOptions);
+
+            return azureOptions;
+        }
+    }
+}
